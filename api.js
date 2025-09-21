a371be452104332e11f03baa831c1ff63d437c6a
avoidcode <xurround@gmail.com>
2025-09-21 22:19:12 +0300

    refactoring

--- api.js
+++ api.js
@@ -12,8 +12,10 @@ const common_fetch_params = {
   "cache": "no-cache"
 };
 
+const BASE_URL = "https://attendance.mirea.ru";
+
 function checkAuth(statusCallback) {
-  fetch("https://attendance.mirea.ru/rtu_tc.rtu_attend.app.UserService/GetMeInfo", {
+  fetch(BASE_URL + "/rtu_tc.rtu_attend.app.UserService/GetMeInfo", {
     ...common_fetch_params,
     "body": "\u0000\u0000\u0000\u0000F\n\u001fhttps://attendance-app.mirea.ru\u0012!\n\u001fhttps://attendance-app.mirea.ru\u0018\u0001",
   }).then(async (r) => {
@@ -29,7 +31,7 @@ function checkAuth(statusCallback) {
 }
 
 function requestApprove(token, statusCallback) {
-  fetch("https://attendance.mirea.ru/rtu_tc.attendance.api.StudentService/SelfApproveAttendance", {
+  fetch(BASE_URL + "/rtu_tc.attendance.api.StudentService/SelfApproveAttendance", {
     ...common_fetch_params,
     "body": "\u0000\u0000\u0000\u0000&\n$" + token,
   }).then((r) => statusCallback(true)).catch((e) => statusCallback(false));
